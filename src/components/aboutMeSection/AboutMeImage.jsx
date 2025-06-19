118c9e832e2c43bf6f31902854f5fc3fcc19f3ef
Felix Octaniel Telaumbanua <felixoctanieltel05@gmail.com>
2025-06-20 02:11:40 +0700

    Update AboutMeImage.jsx

--- src/components/aboutMeSection/AboutMeImage.jsx
+++ src/components/aboutMeSection/AboutMeImage.jsx
@@ -3,11 +3,8 @@ const AboutMeImage = () => {
     <div className="h-[500px] w-[300px] relative">
       <div className="h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden">
         <img
-<<<<<<< HEAD
+
           src="images/about.png"
-=======
-          src="images/about.png"
->>>>>>> 7970545bc7da7e8ec2b19e25fce65337be485146
           alt="About Me Image"
           className="h-full w-auto object-cover"
         />
