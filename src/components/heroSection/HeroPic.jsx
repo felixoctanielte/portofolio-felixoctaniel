ab3dfab84e867aebec51697474b14b5719d1d651
Felix Octaniel <felixoctanieltel05@gmail.com>
2025-06-20 01:59:16 +0700

    testing

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -11,7 +11,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/hexagenimage.png"
+        src="images/hexagenimage.png"
         alt="Felix Octaniel Telaumbanua"
         className="max-h-[450px] w-auto"
       />
